08f32a4351bffd91a28a2e1df623e1ca3905ad8e
Martin Trullard <mtrullar@made-f0Br8s16.clusters.42paris.fr>
2024-08-30 14:56:18 +0200

    main

--- srcs/main.c
+++ srcs/main.c
@@ -6,9 +6,22 @@
 /*   By: mtrullar <mtrullar@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/30 14:27:49 by mtrullar          #+#    #+#             */
-/*   Updated: 2024/08/30 14:28:08 by mtrullar         ###   ########.fr       */
+/*   Updated: 2024/08/30 14:54:18 by mtrullar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../incs/minishell.h"
 
+int	main()
+{
+	char	*read;
+	
+	printf("Welcome to MINISHELL\n");
+	read = readline("->minishell : ");
+	while (read)
+	{
+		printf("%s\n", read);
+		free(read);
+		read = readline("->minishell : ");
+	}
+}
