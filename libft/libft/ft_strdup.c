b9b5ff11d5ee21ec3f99be3843a6f1faf6b47379
Martin Trullard <mtrullar@made-f0Cr6s4.clusters.42paris.fr>
2024-10-02 19:20:18 +0200

    variable strdup

--- libft/libft/ft_strdup.c
+++ libft/libft/ft_strdup.c
@@ -6,7 +6,7 @@
 /*   By: mtrullar <mtrullar@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/23 15:42:12 by mtrullar          #+#    #+#             */
-/*   Updated: 2024/09/17 16:41:59 by mtrullar         ###   ########.fr       */
+/*   Updated: 2024/10/02 19:15:54 by mtrullar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -39,13 +39,60 @@ char	*ft_strdup_till_i(char *src, int i)
 	return (result);
 }
 
+static void	bis(char *str, int i, t_nk *check)
+{
+	if (str[i] == 34 && check->i == 1)
+		check->i = 0;
+	else if (str[i] == 34 && check->i == 0)
+		check->i = 1;
+	else if (str[i] == 39 && check->i == 2)
+		check->i = 0;
+	else if (str[i] == 39 && check->i == 0)
+		check->i = 2;
+	else if (str[i] == 92 && check->j == 0)
+		check->j = 1;
+	else if (str[i] == 92 && check->j == 1)
+		check->j = 0;
+	else if (str[i] == ' ' && str[i - 1] && str[i - 1] != ' ')
+		check->k = 0;
+	else if (str[i] == ' ' && str[i - 1] && str[i - 1] == ' ')
+		check->k = 1;
+}
+
+static int	ft_len(char *src)
+{
+	int		i;
+	int		k;
+	t_nk	c;
+
+	c.i = 0;
+	c.j = 0;
+	c.k = 0;
+	k = 0;
+	i = 0;
+	while (src[i])
+	{
+		bis(src, i, &c);
+		if ((src[i] == 34 && c.i == 2) || (src[i] == 39 && c.i == 1)
+			|| (src[i] == ' ' && c.k == 0)
+			|| (src[i] != 34 && src[i] != 39 && src[i] != ' '))
+			k++;
+		i++;
+	}
+	return (k);
+}
+
 char	*ft_strdup_custom(char *src)
 {
 	char	*result;
-	size_t	i;
+	int		i;
 	int		k;
+	t_nk	c;
 
-	i = ft_custom_len(src);
+	c.i = 0;
+	c.j = 0;
+	c.k = 0;
+	i = ft_len(src);
 	result = malloc(sizeof(char) * (i + 1));
 	if (result == NULL)
 		return (NULL);
@@ -53,11 +100,11 @@ char	*ft_strdup_custom(char *src)
 	k = 0;
 	while (src[i])
 	{
-		if (src[i] != 34 && src[i] != 39)
-		{
-			result[k] = src[i];
-			k++;
-		}
+		bis(src, i, &c);
+		if ((src[i] == 34 && c.i == 2) || (src[i] == 39 && c.i == 1)
+			|| (src[i] == ' ' && c.k == 0)
+			|| (src[i] != 34 && src[i] != 39 && src[i] != ' '))
+			result[k++] = src[i];
 		i++;
 	}
 	result[k] = '\0';
--- srcs/parsing/handle_variable.c
+++ srcs/parsing/handle_variable.c
@@ -6,7 +6,7 @@
 /*   By: mtrullar <mtrullar@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/04 16:20:25 by mtrullar          #+#    #+#             */
-/*   Updated: 2024/10/01 18:35:27 by mtrullar         ###   ########.fr       */
+/*   Updated: 2024/10/02 19:02:45 by mtrullar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -40,11 +40,11 @@ int	ft_check_variable(char *str, t_data *data)
 	t2 = ft_strchr(str, 39);
 	if ((t1 && tmp > t1) || (t2 && tmp > t2) || ft_check_quote_syntax(tmp + 1))
 		return (0);
+	if (!ft_add_variable(str, data))
+		return (1);
 	if (!env_update(data->envs->env, str))
 		return (env_update(data->envs->exp, str), envtab_update(data), 1);
 	if (!env_update(data->envs->exp, str))
-		return (1);
-	if (!ft_add_variable(str, data))
 		return (1);
 	return (0);
 }
